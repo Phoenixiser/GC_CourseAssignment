8af91d4c6860dd8445828c4d21d034cfa3b0a267
Phoenixiser <joseph.h.horton@gmail.com>
2014-11-23 17:08:45 -0500

    optimized

--- run_analysis.R
+++ run_analysis.R
@@ -1,75 +1,74 @@
 # You should create one R script called run_analysis.R that does the following.
 
 # 1 Merges the training and the test sets to create one data set.
-
 # 2 Extracts only the measurements on the mean and standard deviation for each
 # measurement.
-
 # 3 Uses descriptive activity names to name the activities in the data set
-
 # 4 Appropriately labels the data set with descriptive variable names. 
-
 # 5 From the data set in step 4, creates a second, independent tidy data set with
 # the average of each variable for each activity and each subject.
 
-setwd("~/Desktop/Coursera/Getting and Cleaning Data/CourseAssignment/GC_CourseAssignment/")
+features = read.csv("./data/UCI HAR Dataset 2//features.txt", 
+                       sep=" ",
+                       header=FALSE, 
+                       stringsAsFactors=FALSE,
+                       col.names = c("feature.row", "feature.name"))
 
-featureList = read.csv("./data/UCI HAR Dataset 2//features.txt", 
-               sep=" ",
-               header=FALSE, 
-               stringsAsFactors=FALSE,
-               col.names = c("feature.code", "feature"))
 
-activityCode = read.csv("./data/UCI HAR Dataset 2//activity_labels.txt",
+
+activities = read.csv("./data/UCI HAR Dataset 2//activity_labels.txt",
                         sep=" ",
                         header=FALSE, 
                         stringsAsFactors=FALSE,
                         col.names = c("activity.code", "activity"))
 
-featureLength <- length(featureList$feature)
+# Merges the training and the test sets to create one data set.
 
-## Train
-train_x = read.csv.sql("./data/UCI HAR Dataset 2/train/X_train.txt", 
-                       sql = "select 'tBodyAcc-mean()-X' from file",
-                       header=FALSE)
+## X
+train_x = read.table("./data/UCI HAR Dataset 2/train/X_train.txt",
+                     col.names=features$feature.name)
 
-train_x = read.fwf("./data/UCI HAR Dataset 2/train/X_train.txt", 
-                   widths=rep(16, times=10), 
-                   col.names=ftrs$features[1:10],
-                   header=FALSE)
+test_x = read.table("./data/UCI HAR Dataset 2/test/X_test.txt",
+                    col.names=features$feature.name)
 
+all_x = rbind(train_x, test_x)
+
+## Y
 train_y = read.csv("./data/UCI HAR Dataset 2/train/y_train.txt", 
                    header=FALSE, 
                    stringsAsFactors=FALSE,
                    col.names="activity")
 
+test_y = read.csv("./data/UCI HAR Dataset 2/test/y_test.txt", 
+                  header=FALSE, 
+                  stringsAsFactors=FALSE,
+                  col.names="activity")
+
+all_y = rbind(train_y, test_y)
+
+## Subjects
 train_subj = read.csv("./data/UCI HAR Dataset 2/train/subject_train.txt", 
                       header=FALSE, 
                       stringsAsFactors=FALSE,
                       col.names="subject")
-
-train = cbind(train_subj, train_y, train_x)
-
-## Test
 
 test_subj = read.csv("./data/UCI HAR Dataset 2/test/subject_test.txt", 
                       header=FALSE, 
                       stringsAsFactors=FALSE,
                       col.names="subject")
 
-test_y = read.csv("./data/UCI HAR Dataset 2/test/y_test.txt", 
-                   header=FALSE, 
-                   stringsAsFactors=FALSE,
-                   col.names="activity"
-)
+all_subj = rbind(train_subj, test_subj)
+  
+all_data = cbind(all_subj, all_y, all_x)
 
-test_x = read.fwf("./data/UCI HAR Dataset 2/test/X_test.txt", 
-                   widths=rep(16, times=featureLength), 
-                   header=FALSE)
+# 2 Extracts only the measurements on the mean and standard deviation for each
+# measurement.
 
-test = cbind(test_subj, test_y, test_x)
+# 3 Uses descriptive activity names to name the activities in the data set
+all_data$activity = factor(all_data$activity, labels=activities$activity)
 
-# Merges the training and the test sets to create one data set.
-all_data <- rbind(train, test)
+# 4 Appropriately labels the data set with descriptive variable names. 
 
-#write
+# 5 From the data set in step 4, creates a second, independent tidy data set with
+# the average of each variable for each activity and each subject.
+
